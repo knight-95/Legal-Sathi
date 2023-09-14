5a21be52fce72675764f80e377078948948fb0ef
Yash Gupta <yashgupta13.in@gmail.com>
2023-09-15 02:37:59 +0530

    completed profileCard, added marquee section

--- src/Components/Cards/ProfileCards.tsx
+++ src/Components/Cards/ProfileCards.tsx
@@ -14,8 +14,16 @@ import {
   CardBody,
   Card,
   CardHeader,
+  Tag,
+  Divider,
 } from "@chakra-ui/react";
-
+import { FcVideoCall } from "react-icons/fc";
+import { FiPhoneCall } from "react-icons/fi";
+import { AiOutlineMessage } from "react-icons/ai";
+import { GoBriefcase } from "react-icons/go";
+import { GrLocation } from "react-icons/gr";
+import FlexColumn from "../../_ui/flex/FlexColumn";
+import FlexRow from "../../_ui/flex/FlexRow";
 type Props = {
   index?: string;
   title?: string;
@@ -40,53 +48,77 @@ export default function ProfileCards({
   height,
 }: Props) {
   return (
-    <Card data-type="Card" maxW="md">
-      <CardHeader data-type="CardHeader">
-        <Flex data-type="Flex">
-          <Flex
-            data-type="Flex"
-            flex="1"
-            gap="4"
-            alignItems="center"
-            flexWrap="wrap"
-          >
-            <Avatar
-              data-type="Avatar"
-              name="Segun Adebayo"
-              src="https://images.unsplash.com/photo-1531384441138-2736e62e0919?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzF8fHBvcnRyYWl0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-            ></Avatar>
-            <Box data-type="Box">
-              <Heading data-type="Heading" size="sm">
-                Aloïs Pierre
-              </Heading>
-              <Text data-type="Text">Creator of Studio 23</Text>
-            </Box>
-          </Flex>
-        </Flex>
-      </CardHeader>
-      <CardBody data-type="CardBody">
-        <Text data-type="Text">
-          With Chakra UI, I wanted to sync the speed of development with the
-          speed of design. I wanted the developer to be just as excited as the
-          designer to create a screen.
-        </Text>
-      </CardBody>
-      <Image
-        data-type="Image"
-        objectFit="cover"
-        src="https://images.unsplash.com/photo-1546815670-5c5e437ffc6d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8c3RhcmJ1Y2tzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-        alt="Starbucks"
-      ></Image>
+    <Card maxW="md" marginLeft="1.5rem">
+      <FlexRow>
+        <FlexColumn>
+          <CardHeader data-type="CardHeader">
+            <Flex
+              data-type="Flex"
+              flex="1"
+              gap="4"
+              alignItems="center"
+              flexWrap="wrap"
+            >
+              <Avatar
+                data-type="Avatar"
+                name="Segun Adebayo"
+                src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ficon%2Favatar_147142&psig=AOvVaw1tcurMCPH2PWoYhjVzNfqJ&ust=1694806465367000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCNiQ2p7sqoEDFQAAAAAdAAAAABAD"
+              ></Avatar>
+              <Box data-type="Box">
+                <Heading data-type="Heading" size="sm">
+                  Arya Rajput
+                </Heading>
+                <Text color="green">Available</Text>
+              </Box>
+            </Flex>
+          </CardHeader>
+          <Box padding="1.25rem">
+            <Flex alignItems="center">
+              <GrLocation />
+              <Text marginLeft="0.5rem">Indore, Madhya Pradesh</Text>
+            </Flex>
+            <Flex alignItems="center">
+              <GoBriefcase />
+              <Text marginLeft="0.5rem">7 Years of Experience</Text>
+            </Flex>
 
-      <Flex marginTop="2rem" justifyContent="flex-end" paddingBottom="2rem">
-        <Box justifyContent="space-between">
-          <Button colorScheme="green" variant="outline">
-            Call Now
-          </Button>
+            <Flex alignItems="center">
+              <AiOutlineMessage />
+              <Text marginLeft="0.5rem">English, Hindi</Text>
+            </Flex>
+          </Box>
+        </FlexColumn>
+        <FlexColumn></FlexColumn>
+      </FlexRow>
+
+      <Box justifyContent="space-between" padding="1.25rem">
+        <Text fontWeight="500" fontSize="1.2rem" marginBottom="2px">
+          Practice areas and Skills:
+        </Text>
+        <Tag marginRight="0.2rem">Civil Matters</Tag>
+        <Tag marginRight="0.2rem">Property & Real State</Tag>
+        <Tag marginRight="0.2rem">Cyber Crime</Tag>
+      </Box>
+      <Center>
+        <Divider orientation="horizontal" width="80%" />{" "}
+      </Center>
+
+      <Flex
+        marginTop="1rem"
+        marginRight="1rem"
+        justifyContent="flex-end"
+        paddingBottom="2rem"
+      >
+        <Stack spacing={3} direction="row">
           <Button colorScheme="blue" variant="outline">
-            Video Call
+            <FiPhoneCall />
+            <Text marginLeft="0.5rem">Call Now</Text>
           </Button>
-        </Box>
+          <Button colorScheme="green" variant="outline">
+            <FcVideoCall />
+            <Text marginLeft="0.5rem">Video Call</Text>
+          </Button>
+        </Stack>
       </Flex>
     </Card>
   );
